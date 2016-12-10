b96939b84f562de25ed5373b0b3ea52e69ecae99
Christian Hackl <chack@live.at>
2016-12-10 21:43:58 +0100

    Initial Commit

---
+++ Webradio/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Webradio
+//
+//  Created by Christian Hackl on 10/12/2016.
+//  Copyright © 2016 Christian Hackl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
